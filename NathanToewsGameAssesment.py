ca13b7322205fc02f7009b7659c62aebbccb6a40
nathan21153 <164109270+nathan21153@users.noreply.github.com>
2024-04-03 18:58:03 +1300

    at school
    aril 3rd

--- NathanToewsGameAssesment.py
+++ NathanToewsGameAssesment.py
@@ -1,37 +1,66 @@
-#
 #variables
-#
+#---------
 import time
 import random
-choose_start_or_menu=0 #this is used to let phython know whether it should run start screen or ingame menu, this save lines of code (needs less functions)
-c_or_s="start" #continue or start for what 
-#
+s_or_p_menu=0 #this is used to let phython know whether it should run start or pause in the menus function, this save lines of code (needs less functions)
+c_or_s=["start", "continue"] #continue or start for what ever menu your in
+
 #lists and hashes
-#
+#----------------
 
-#
 #functions
-#
-def main_menus (t, y): 
-    print(continue_or_start)
-    if t==0:
-        print("----------------------------------")
-        print("Welcome subnautica below graphics!")
-        print("----------------------------------")
-        print()
-        choose_start_or_menu=1
-
-    elif choose_start_or_menu==1:
-        print("----------------------------------")
+#---------
+def menus (): #pause and open screen menu
+    global s_or_p_menu
+    global c_or_s
+    option=0 #option variable within each function so it can be reused and just redefines itself
+    if s_or_p_menu == 0:
+        print("""
+----------------------------------
+Welcome subnautica below graphics!
+----------------------------------
+To select options in each menu:
+input corresponding number
+        """)
+        print("start [1]")
+    elif start_or_p_menu==1:
+        print("-----------")
         print("Game paused")
-        print()
-
-    print("Select an option (input corresponding number):")
-    print("Start, 1")
-    print("Help, 2")
-    print("")
-#
+        print("coninue [1]")
+    print("Help [2]")
+    print("quit game [3]")
+    print("Select an option:")
+    option=int(input())
+    while True:
+        try:
+            if option == 1:
+                begining()
+            elif option == 2:
+                print("""
+Help:
+------------------------------------------------------------
+To select options in each menu:
+input corresponding number type option you wish to choose
+------------------------------------------------------------
+continue? yes[1] quit [2]
+    """)
+                option=int(input())
+            elif option == 3:
+                quit
+            else:
+                print("""
+MISS INPUT!! Please try again
+type desierd option or input corresponding number:
+    """)
+        
+        except ValueError:
+            print("""
+MISS INPUT!! Please try again
+type desierd option or input corresponding number:
+    """)
+def begining ():
+    print ("placeholder")
+    
 #main routine
-#
-print()
-start_screen_or_ingame_menu (choose_start_or_menu, gay)
+#------------
+menus()
