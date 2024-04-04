da581f64d40fe351dac50662b8e42ff46813cf9c
nathan21153 <164109270+nathan21153@users.noreply.github.com>
2024-04-05 00:17:23 +1300

    changes 5th april at home

--- NathanToewsGameAssesment.py
+++ NathanToewsGameAssesment.py
@@ -2,67 +2,51 @@
 #---------
 import time
 import random
-s_or_p_menu=0 #this is used to let phython know whether it should run start or pause in the menus function, this save lines of code (needs less functions)
-c_or_s=["start [1]", "continue [1]"] #continue or start for what ever menu your in
+
 
 #lists and hashes
 #----------------
 
 #functions
 #---------
-def menus (): #pause and open screen menu
-    global s_or_p_menu
-    global c_or_s
-    option=0 #option variable within each function so it can be reused and just redefines itself
-    if s_or_p_menu == 0:
-        s_or_p_menu+=1
-        print("""
-----------------------------------
-Welcome subnautica below graphics!
-----------------------------------
-To select options in each menu:
-input corresponding number
-        """)
-        print("start [1]")
-    elif start_or_p_menu==1:
-        print("-----------")
-        print("Game paused")
-        print("coninue [1]")
-    print("Help [2]")
-    print("quit game [3]")
-    print("Select an option:")
-    option=int(input())
+def start_menu (): #pause and open screen menu
+    print ("""
+-------------------------------------
+Welcome to subnautica below graphics!
+-------------------------------------
+How to play:
+when an option show up you can press thecorresponding
+number in the square brackets ([]) next to that option
+""")
     while True:
-        if option == 1:
-            begining()
-        elif option == 2:
-            print("""
-Help:
-------------------------------------------------------------
-To select options in each menu:
-input corresponding number type option you wish to choose
-------------------------------------------------------------
-continue? yes[1] quit [3]
-            """)
-
+        option=0 #option variable within each function so it can be reused and just redefines itself
+        print("start game [1]")
+        print("quit game [2]")
+        print("Select an option:")
+        try:
             option=int(input())
-        elif option == 3:
-            quit
-            
-        else:
+            if option == 1:
+                begining()
+                break
+            elif option == 2:
+                quit ()
+
+        except ValueError:
             print("""
 MISS INPUT!! Please try again
 type desierd option or input corresponding number:
             """)
-            print(c_or_s[s_or_p_menu])
-            print("Help [2]")
-            print("quit game [3]")
-            print("Select an option:")
-            option=int(input())
+            continue #goes back to top of loop
+            
+        else: #checks for number outside of options
+            print("""
+MISS INPUT!! Please try again
+type desierd option or input corresponding number:
+            """)
 
 def begining ():
-    print ("placeholder")
+    print("placeholder")
     
 #main routine
 #------------
-menus()
+start_menu()
